1b22136e088a0eeacec863d5e7b42888c3c079e1
Dmytro Uglach <dmitryuglach@gmail.com>
2024-11-10 19:09:59 +0200

    classes

--- Desk.py
+++ Desk.py
@@ -1,13 +1,18 @@
+import random
 from dataclasses import dataclass
-from enum import Enum
-from typing import List
+from enum import IntEnum
+from typing import List, Tuple, Dict
 
-class Directions(Enum):
+
+class Directions(IntEnum):
     IDLE = 0
     UP = 1
     RIGHT = 2
     DOWN = 4
     LEFT = 8
+
+opposits = {Directions.UP: Directions.DOWN, Directions.DOWN: Directions.UP, Directions.LEFT: Directions.RIGHT, Directions.RIGHT: Directions.LEFT}
+movements: Dict[Directions, Tuple[int, int]] = {Directions.IDLE: (0, 0), Directions.UP: (0, -1), Directions.RIGHT: (1, 0), Directions.DOWN: (0, 1), Directions.LEFT: (-1, 0)}
 
 @dataclass
 class Cell:
@@ -23,5 +28,34 @@ class Desk:
         self.height = height
         self.init_desk()
 
+    def __getitem__(self, item):
+        return self.desk[item]
+
     def init_desk(self):
-        self.desk = [[Cell() for y in range(self.height)] for x in range(self.width)]
+        self.desk = [[Cell() for _ in range(self.height)] for _ in range(self.width)]
+
+    def move(self, x: int, y: int, direction: Directions = Directions.IDLE) -> Tuple[int, int]:
+        return x + movements[direction][0], y + movements[direction][1]
+
+    def is_empty(self, x: int, y: int, direction: Directions = Directions.IDLE) -> bool:
+        (x, y) = self.move(x, y, direction)
+        res = self.width > x >= 0 and 0 <= y < self.height
+        return res and 0 == self.desk[x][y].ways
+
+    def move_rand(self, x, y) -> Tuple[int, int] | None:
+        directions = [Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT]
+        directions = list(filter(lambda i: (self.desk[x][y].ways & int(i) == 0), directions))
+
+        while len(directions) > 0:
+            direction = random.choice(directions)
+            if self.is_empty(x, y, direction):
+                self.desk[x][y].ways |= direction
+                (x, y) = self.move(x, y, direction)
+                self.desk[x][y].ways |= opposits[direction]
+                return x, y
+
+            directions.remove(direction)
+
+        self.desk[x][y].fixed = True
+        return None
+
--- Render.py
+++ Render.py
@@ -1,8 +1,6 @@
 import random
-
 import pygame
-
-from Desk import Desk
+from Desk import Desk, Directions
 
 
 class Render:
@@ -14,10 +12,13 @@ class Render:
         pygame.display.set_caption("Hello Maze")
         self.screen.fill((0, 0, 0))
 
+    def quit(self):
+        pygame.quit()
+
     def draw(self, desk: Desk):
         self.screen.fill((0, 0, 0))
-        for x in range(len(desk.desk)):
-            for y in range(len(desk.desk[x])):
+        for x in range(desk.width):
+            for y in range(desk.height):
                 top_left = (x * self.width + self.offset[0], y * self.width + self.offset[1])
                 top_right = (x * self.width + self.offset[0] + self.width, y * self.width + self.offset[1])
                 bottom_left = (x * self.width + self.offset[0], y * self.width + self.offset[1] + self.width)
@@ -27,17 +28,17 @@ class Render:
                 brick_color = (0, 0, 0) if desk[x][y].fixed else (100, 0, 0)
                 brick_color = (30, 30, blue_effect) if desk[x][y].ways == 0 else brick_color
 
-                pygame.draw.rect(screen, brick_color, pygame.Rect(top_left[0], top_left[1], width, width))
+                pygame.draw.rect(self.screen, brick_color, pygame.Rect(top_left[0], top_left[1], self.width, self.width))
 
-                if not desk[x][y].ways & UP:
-                    pygame.draw.line(screen, color, top_left, top_right, width=1)
+                if not desk[x][y].ways & Directions.UP:
+                    pygame.draw.line(self.screen, color, top_left, top_right, width=1)
 
-                if not desk[x][y].ways & RIGHT:
-                    pygame.draw.line(screen, color, top_right, bottom_right, width=1)
+                if not desk[x][y].ways & Directions.RIGHT:
+                    pygame.draw.line(self.screen, color, top_right, bottom_right, width=1)
 
-                if not desk[x][y].ways & DOWN:
-                    pygame.draw.line(screen, color, bottom_right, bottom_left, width=1)
+                if not desk[x][y].ways & Directions.DOWN:
+                    pygame.draw.line(self.screen, color, bottom_right, bottom_left, width=1)
 
-                if not desk[x][y].ways & LEFT:
-                    pygame.draw.line(screen, color, top_left, bottom_left, width=1)
+                if not desk[x][y].ways & Directions.LEFT:
+                    pygame.draw.line(self.screen, color, top_left, bottom_left, width=1)
         pygame.display.update()
